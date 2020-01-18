be86822a4895df6ce8934b608e9cfa27a8d037f8
chris-pan <55161067+chris-pan@users.noreply.github.com>
2020-01-18 14:21:33 -0800

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import json
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-api_key = 'AIzaSyAaBPmBUlMNxdwEmtTDlBCpHDo05b_WjpQ'
-
+api_key = 'API-KEY'
 def main():
     userQuery = 'In Congress, Elizabeth Warren introduced 110 bills. 2 passed. Cory Booker introduced 120 bills. 0 passed. Kamala Harris introduced 54 bills. 0 passed. Bernie Sanders never held a job until age 53. He lived off of welfare and four different women.'
     factCheckService = build("factchecktools", "v1alpha1", developerKey=api_key)
